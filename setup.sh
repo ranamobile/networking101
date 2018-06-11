bfc6cf734ce6bdb2c7a0c4682861b8f6d96384a8
Ariana Hong <ahong@farm.local.parsons.com>
2018-06-10 20:31:11 -0400

    Automatically configure the base Debian VM to enable IP forwarding

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #
 # Set up the Networking 101 training.
+cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd
+source functions.sh
 
 # Install VirtualBox
 sudo yum install -y VirtualBox-5.2
@@ -19,3 +21,12 @@ fi
 while [ $(vboxmanage list hostonlyifs | grep -E ^Name | wc -l) -lt 5 ]; do
   vboxmanage hostonlyif create
 done
+
+# Set up Debian VM to allow IPv4 traffic through
+startvm Debian-7.8-amd64-minimal
+waitforvm Debian-7.8-amd64-minimal
+sendcmd Debian-7.8-amd64-minimal /bin/sed -i "s/#net.ipv4.ip_forward/net.ipv4.ip_forward/g" /etc/sysctl.conf
+sendcmd Debian-7.8-amd64-minimal /sbin/sysctl -p
+sendcmd Debian-7.8-amd64-minimal /sbin/poweroff
+
+exit 0
